80607d7a5b03018a1fe402d9f9e728ed44ee588b
李京伟 <ljwmap@hotmail.com>
2020-02-16 21:42:14 +0800

    增加快捷菜单和主窗口

--- FutureTools/App.xaml.cs
+++ FutureTools/App.xaml.cs
@@ -5,13 +5,27 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace FutureTools
 {
     /// <summary>
-    /// App.xaml 的交互逻辑
+    /// 系统应用
     /// </summary>
     public partial class App : Application
     {
+        private TaskbarIcon notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            notifyIcon.Dispose();
+            base.OnExit(e);
+        }
     }
 }
---
+++ FutureTools/NotifyIconViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FutureTools
+{
+    public class NotifyIconViewModel
+    {
+        /// <summary>
+        /// 显示窗口
+        /// </summary>
+        public ICommand ShowWindowCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CanExecuteFunc = () => Application.Current.MainWindow == null,
+                    CommandAction = () =>
+                    {
+                        Application.Current.MainWindow = new MainWindow();
+                        Application.Current.MainWindow.Show();
+                    }
+                };
+            }
+        }
+
+        /// <summary>
+        /// 隐藏窗口
+        /// </summary>
+        public ICommand HideWindowCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CommandAction = () => Application.Current.MainWindow.Close(),
+                    CanExecuteFunc = () => Application.Current.MainWindow != null
+                };
+            }
+        }
+
+
+        /// <summary>
+        /// 关闭应用
+        /// </summary>
+        public ICommand ExitApplicationCommand
+        {
+            get
+            {
+                return new DelegateCommand {CommandAction = () => Application.Current.Shutdown()};
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 命令代理
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        public Action CommandAction { get; set; }
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        public void Execute(object parameter)
+        {
+            CommandAction();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null  || CanExecuteFunc();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
